e9344071d9f7b6a5f88f29046acd0cca231d8e99
lx-abe <hayato.abe@luxiar.com>
2020-03-09 16:40:13 +0900

    [管理側]作業場所削除機能を追加する

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :stations
   resources :workshops, only: [:index, :show]
   namespace :admin do
-    resources :workshops, only: [:index, :new, :create, :edit, :update]
+    resources :workshops, except: :show
   end
 end

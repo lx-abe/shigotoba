b86cb4c90e3b1702f1fedbddb7b1184ac93b6159
lx-abe <hayato.abe@luxiar.com>
2020-03-11 11:38:48 +0900

    作業場所検索機能を追加

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -3,10 +3,18 @@
 # 施設閲覧（ユーザー向け）
 class WorkshopsController < ApplicationController
   def index
-    @workshops = Workshop.all
+    @search_params = workshop_search_params
+    @workshops = Workshop.search(@search_params)
+    # binding.pry
   end
 
   def show
     @workshop = Workshop.find(params[:id])
+  end
+
+  private
+
+  def workshop_search_params
+    params.fetch(:search, {}).permit(:station_id, :category, :wifi)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'stations#index'
   resources :stations
-  resources :workshops, only: [:index, :show]
+  resources :workshops, only: [:index, :show] do
+    get :search, on: :collection
+  end
   namespace :admin do
     resources :workshops, except: :show
   end

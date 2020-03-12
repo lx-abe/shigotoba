a24903838f7168ce026c790244da413dc70d03ce
lx-abe <hayato.abe@luxiar.com>
2020-03-12 17:05:59 +0900

    作業場所検索機能をコントローラに再定義

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -4,7 +4,7 @@
 class WorkshopsController < ApplicationController
   def index
     @search_params = workshop_search_params
-    @workshops = Workshop.search(@search_params)
+    @workshops = search(workshop_search_params)
   end
 
   def show
@@ -12,6 +12,37 @@ class WorkshopsController < ApplicationController
   end
 
   private
+
+  def search(search_params)
+    workshops = Workshop.all
+    workshops_searched_by_station_id = search_by_station_id(workshops, search_params[:station_id])
+    workshops_searched_by_category = search_by_category(workshops_searched_by_station_id, search_params[:category])
+    search_by_wifi(workshops_searched_by_category, search_params[:wifi])
+  end
+
+  def search_by_station_id(workshops, station_id)
+    if station_id.present?
+      workshops.where(station_id: station_id)
+    else
+      workshops
+    end
+  end
+
+  def search_by_category(workshops, category)
+    if category.present?
+      workshops.where(category: category)
+    else
+      workshops
+    end
+  end
+
+  def search_by_wifi(workshops, wifi)
+    if wifi
+      workshops.where(wifi: wifi)
+    else
+      workshops
+    end
+  end
 
   def workshop_search_params
     params.fetch(:search, {}).permit(:station_id, :category, :wifi)

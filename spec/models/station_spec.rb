e25de6d43a1bd2dbeb9d44a62f444f4f21ac16f9
lx-abe <hayato.abe@luxiar.com>
2020-03-27 11:16:17 +0900

    テストコード記述効率化

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,16 +2,14 @@
 
 require 'rails_helper'
 
-# 駅名があれば有効
 RSpec.describe Station, type: :model do
+  # 駅名があれば有効
   it 'is valid with name' do
     station = Station.new(name: '武蔵小杉')
     expect(station).to be_valid
   end
-end
 
-# 駅名がなければ無効
-RSpec.describe Station, type: :model do
+  # 駅名がなければ無効
   it 'is invalid without name' do
     station = Station.new(name: nil)
     station.valid?
